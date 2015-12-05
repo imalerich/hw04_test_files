d1726aa5121490841c93115a193021fdec2d0fbf
Ian Michael Malerich <imm@iastate.edu>
2015-12-05 17:31:22 -0600

    rnd229 is now fully functional, may need testing

--- cs229/rnd229/main.cpp
+++ cs229/rnd229/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include <time.h>
+#include <getopt.h>
 
 using namespace std;
 
-string file_name;
-int freq_mult;
+string file_name = "rnd.cs229";
+int freq_mult = 20;
+int min_channels = 1;
+int max_channels = 7;
+int min_length = 1; // seconds
+int max_length = 30; // seconds
+
 int num_channels;
 int bit_res;
 int sample_rate;
@@ -18,17 +26,80 @@ void output_rand_data(ostream &out);
 void print_info();
 void print_help();
 
-int main() {
-	ofstream file;
+int main(int argc, char ** argv) {
+	srand(time(NULL));
 
-	file_name = "rnd.cs229";
-	freq_mult = 20;
+	// grab any arguments for generation
+	static struct option long_options[] = {
+		{ "help", no_argument, 0, 'h' },
+		{ "output", required_argument, 0, 'o' },
+		{ "freq", required_argument, 0, 'f' },
+		{ "min-channels", required_argument, 0, 0 },
+		{ "max-channels", required_argument, 0, 'c' },
+		{ "min-length", required_argument, 0, 0 },
+		{ "max-length", required_argument, 0, 'l' },
+		{ 0, 0, 0, 0 }
+	};
 
-	num_channels = (rand() % 7 + 1); // (1, 7) channels
+	char c = 0;
+	int option_index = 0;
+
+	while ((c = getopt_long(argc, argv, "ho:f:c:l:012", long_options, &option_index)) != -1) {
+		switch(c) {
+			case 0:
+				switch (option_index) {
+				case 3: // min-channels
+					min_channels = atoi(optarg);
+					cout << "min_channels: " << min_channels << endl;
+					break;
+
+				case 5: // min-length
+					min_length = atoi(optarg);
+					cout << "min_length: " << min_length << endl;
+					break;
+				}
+
+				break;
+
+			case 'o':
+				file_name = string(optarg);
+				cout << "file_name: " << file_name << endl;
+				break;
+
+			case 'f':
+				freq_mult = atoi(optarg);
+				cout << "freq_mult: " << freq_mult << endl;
+
+			case 'c':
+				max_channels = atoi(optarg);
+				cout << "max_channels: " << max_channels << endl;
+				break;
+
+			case 'l':
+				max_length = atoi(optarg);
+				cout << "max_length: " << max_length << endl;
+				break;
+				
+			case 'h':
+				print_help();
+				return 0;
+		}
+	}
+
+	// if any extra arguments are found, print the help and exit
+	if (argc - optind > 0) {
+		print_help();
+		return 1;
+	}
+
+	// create the limit values for the generated file
+	num_channels = (rand() % (max_channels - min_channels + 1) + min_channels); // number of channels from (min_channels, max_channels) 
 	bit_res = pow(2, (rand() % 3 + 3)); // 8, 16, or 32
 	sample_rate = (rand() % 4 + 1) * freq_mult; // freq_mult hz * (1, 4)
-	num_samples = sample_rate * (rand() % 30 + 1); // (1, 30) seconds long
+	num_samples = sample_rate * (rand() % (max_length - min_length + 1) + min_length); // (min_length, max_length) seconds long
 
+	// generate the file
+	ofstream file;
 	file.open(file_name);
 	output_header(file);
 	output_rand_data(file);
@@ -80,4 +151,17 @@ void print_info() {
 }
 
 void print_help() {
+		cout << "Usage: rnd229 [options]" << endl;
+		cout << "  -h --help\tDisplay this information" << endl;
+		cout << "  -o --output\tOutput file name (default 'rnd.cs229')" << endl;
+		cout << "  -f --freq\tFrequency multiplier, SampleRate will be a random value in the range (1, 4) times this value (deafault '20')" << endl;
+		cout << "  --min-channels\tMinimum number of channels in the output file (default '1')" << endl;
+		cout << "  -c --max-channels\tMaximum number of channels in the output file (default '7')" << endl;
+		cout << "  --min-length\tMinimum length of generated sound (in seconds) (default '1')" << endl;
+		cout << "  -l --max-length\tMaximum length of generated sound (in seconds) (default '30')" << endl;
+		cout << "Generates a random .cs229 file within the input bounds." << endl;
+		cout << "The output will be written to the specified file (rnd.cs229 if none present) and the appropriate info for 'sndinfo' will be written to the standard output." << endl;
+		cout << "If no arguments are provided, this program will generate the file rnd.cs229 with (1,7) channels, with a length of (1,30) seconds." << endl;
+		cout << "BitResolution will either be 8, 16, or 32 bits. Samples are generated at random to fit within that bit depth." << endl;
+		cout << "This program was written by Ian Malerich for testing hw04 of cs229 at Iowa State University (Fall 2015)." << endl;
 }
