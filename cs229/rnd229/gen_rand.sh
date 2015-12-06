4406384d73ff32b64124c85ea4c7729f9fc813ce
Ian Malerich <ian.malerich@gmail.com>
2015-12-06 22:41:43 +0000

    sleeping for a second for a new seed in the random number generator

--- cs229/rnd229/gen_rand.sh
+++ cs229/rnd229/gen_rand.sh
@@ -3,4 +3,6 @@
 for VAR in {1..20}
 do
 	./rnd229 -o rnd$VAR.cs229 -f 100 --min-channels 4 --min-length 60 -l 120
+	# files are seeded with the current time (seconds), make sure they are different
+	sleep 1s
 done
